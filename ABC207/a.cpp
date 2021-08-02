7decc83ec5e717c1e1375daec76c46da84743ca8
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-08-02 19:19:11 +0900

    ABC207

---
+++ ABC207/a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n)
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll, ll>;
+using Graph = vector<vector<int>>;
+using Edge = pair<int, ll>;
+const ll INF = 1LL << 60;
+const int MAX = 1e5 + 10;
+const int MOD = 1000000007;
+
+int main()
+{
+    // cin高速化
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a >= c && b >= c)
+        cout << a + b << "\n";
+    else if (a >= b && c >= b)
+        cout << a + c << "\n";
+    else if (c >= a && b >= a)
+        cout << b + c << "\n";
+    else
+        cout << a + b << "\n";
+    return 0;
+}
